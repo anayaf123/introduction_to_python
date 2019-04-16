7f232a9551cd7a032e5bde9182c21dac055d3299
WhosBrandon1092 <49209995+WhosBrandon1092@users.noreply.github.com>
2019-04-16 15:57:04 -0400

    Update hello_world_0.0.py

--- hello_world/hello_world_0.0.py
+++ hello_world/hello_world_0.0.py
@@ -1,1 +1,4 @@
-print()
+print("Hello World!")
+print("Hello Person!")
+print("Hello Sir!")
+print("Hello Mam!")
---
+++ input_output/input_output0.0.py
@@ -0,0 +1,14 @@
+# This is a comment in Python. hi hello
+# Generally comments are used to explain how code works. 
+
+print("Hello, how are you?")
+('What is your name?')
+
+# In Python, you use variables to store data. 
+# Variable names should tell you what data they store. 
+# The next line will have the user input their name.  What variable name could you use here?
+name= input("Im Good, My name is Brandon!")
+
+# The next line should print their name back to the screen. 
+
+print("Brandon")
