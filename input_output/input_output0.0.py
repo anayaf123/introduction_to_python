7a7b726ae43de228a46d468d6ae28d78721e2189
s20014067 <49455500+s20014067@users.noreply.github.com>
2019-04-17 14:49:07 -0400

    Update input_output0.0.py
    I had a little bit of trouble

--- input_output/input_output0.0.py
+++ input_output/input_output0.0.py
@@ -1,14 +1,15 @@
 # This is a comment in Python.  Change this to say something else.
 # Generally comments are used to explain how code works. 
 
-print(Hello, how are you?)
-('What is your name?')
+print("Hello, how are you?")
+print('What is your name?')
 
 # In Python, you use variables to store data. 
 # Variable names should tell you what data they store. 
 # The next line will have the user input their name.  What variable name could you use here?
- = input() 
+ = input("good") 
+ = input("nehemiah") 
 
 # The next line should print their name back to the screen. 
-
-print(WHAT GOES HERE?)
+= input("good") 
+print('nehemiah?')
