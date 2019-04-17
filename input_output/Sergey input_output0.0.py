cfde87c2f5b1f2d95ac3cc5b0e0b4d999575de2b
Sergey <s11575552@students.duvalschools.org>
2019-04-17 16:05:16 -0400

    Adding input output code
    output input code

---
+++ input_output/Sergey input_output0.0.py
@@ -0,0 +1,13 @@
+# This is a comment in Python.  Change this to say something else.
+# Generally comments are used to explain how code works. 
+
+print('What is your name?')
+
+# In Python, you use variables to store data. 
+# Variable names should tell you what data they store. 
+# The next line will have the user input their name.  What variable name could you use here?
+x = input() 
+
+# The next line should print their name back to the screen. 
+
+print("Hello," + x)
