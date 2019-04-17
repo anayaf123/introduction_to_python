032f175b35d295b3399da2a379bcadc3ed5d83ec
Jadin Rose <s20051643@students.duvalschools.org>
2019-04-17 13:09:44 -0400

    test

--- hello_world/hello_world_0.1a.py
+++ hello_world/hello_world_0.1a.py
@@ -1,2 +1,3 @@
-# This is a comment.  Add a comment with your FIRST NAME and LAST INITIAL and the DATE YOU WRITE THE CODE. 
-print(FIGURE OUT WHAT GOES HERE)WHAT GOES AT THE END? 
+# !/usr/bin/python
+# Hello World 
+print( 'Hello World' )
