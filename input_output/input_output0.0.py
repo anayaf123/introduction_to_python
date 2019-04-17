26085eb03cbeb347ab9bba27f759da83d0faec51
Cavespider386 <49206765+Cavespider386@users.noreply.github.com>
2019-04-17 11:44:15 -0400

    Update input_output0.0.py

--- input_output/input_output0.0.py
+++ input_output/input_output0.0.py
@@ -1,14 +1,2 @@
-# This is a comment in Python.  Change this to say something else.
-# Generally comments are used to explain how code works. 
-
-print(Hello, how are you?)
-('What is your name?')
-
-# In Python, you use variables to store data. 
-# Variable names should tell you what data they store. 
-# The next line will have the user input their name.  What variable name could you use here?
- = input() 
-
-# The next line should print their name back to the screen. 
-
-print(WHAT GOES HERE?)
+print('I love {1} and {0}'.format('bread','butter'))
+print('I love {0} and {1}'.format('bread','butter'))
