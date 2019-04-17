53d1b92cd93233310927d413aa473d38c858cedf
Kat <49570315+Pepper9001@users.noreply.github.com>
2019-04-17 15:05:18 -0400

    created hello_world\hello_world_0.1a.py
    Learning how to commit to github

--- input_output/input_output0.1.py
+++ input_output/input_output0.1.py
@@ -1,14 +1,16 @@
 # This is a comment in Python.  Change this to say something else.
 # Generally comments are used to explain how code works. 
 
-print(Hello, how are you?)
-('What is your name?')
+print('What is your name?')
 
 # In Python, you use variables to store data. 
-# Variable names should tell you what data they store. 
+# Variable names should tell you what data they store.
+# Variables can be written as camelCase or snake_case. 
 # The next line will have the user input their name.  What variable name could you use here?
- = input() 
 
+VARIABLE_HERE = input()
+variablelHere  =  
+print("You are", VARIABLE_NAME_HERE, "years old?")
 # The next line should print their name back to the screen. 
+print()
 
-print(WHAT GOES HERE?)
