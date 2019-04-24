f28a53601ee856225c1fd2ad16ba497f6a405387
Jewel2143 <s11586674@student.duvalschools.org>
2019-04-24 11:13:50 -0400

    zeldaness
    zeldazeldazelda

---
+++ hello_world/zelda thing.py
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html> 
+
+<html> 
+
+<head> 
+
+<title>zelda thing </title> 
+
+<body> 
+
+<h1>dis is zelda thing</h1> 
+
+ <img src=https://watchusplaygames.files.wordpress.com/2013/03/the-legend-of-zelda-skyrule.jpg/> 
+
+  
+
+ <p> 
+
+  
+
+<body> 
