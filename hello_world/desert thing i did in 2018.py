1fb4f60b4a7e589265a6bee039dcabaceff34d5a
Jewel2143 <s11586674@student.duvalschools.org>
2019-04-24 11:04:37 -0400

    Update desert thing i did in 2018.py

--- hello_world/desert thing i did in 2018.py
+++ hello_world/desert thing i did in 2018.py
@@ -1,39 +1,16 @@
 
 <!DOCTYPE html> 
-
 <html> 
-
-<head>
-
- <title>Recipe Central vol.2</title> 
-
+<head> 
+<title>this is how u do a tittle </title> 
 <body> 
-
-<h1>Desert of the week</h1> 
-
- <p>the desert is a fall deserts in spirt of fallolloween</p> 
-
- <p>Ingredeents:plain cupcake,white frosting,chocolate chips,foil and skills.</p> 
- 
-<p>STEP1)Put the white frosting in the piper (make shere you use acirculer piper shape).</p> 
-
-<p>STEP2)Pipe a perimed shaped sweerl of whiteness. </p> 
-
-<p>STEP3)Take your minnie choclate chips and press the tips in the white frosting like eyes.</p> 
-
-<p>STEP4)Take the normal sized choclate chip and stick the tip under the other to ones </p> 
-
-<p>STEP5)Eat it :)</p> 
-
-<img src="https://www.frugalcouponliving.com/wp-content/uploads/2015/08/ghost-cupcakes-frugal-coupon-living.jpg"height=415 width=320 /> 
-
+<h1>this is how u do a heading</h1> 
+<p> this is how u put stuff in the middle</p> 
+<p>and don’t forget to put a <body> at the bottom like shown</p> 
+<img src="https://www.frugalcouponliving.com/wp-content/uploads/2015/08/ghost-cupcakes-frugal-coupon-living.jpg"height=415 width=320 />
+<p>above is how u do an image</p> 
 <p> 
 
   
 
 <body>  
-
- 
-
-
-
