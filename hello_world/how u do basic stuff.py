542f7bbd164e829b1b7588d1cc4e4c79fff60329
Jewel2143 <s11586674@student.duvalschools.org>
2019-04-24 11:04:30 -0400

    how u do basic stuff
    stuff

---
+++ hello_world/how u do basic stuff.py
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>this is how u do a tittle </title> 
+<body> 
+<h1>this is how u do a heading</h1> 
+<p> this is how u put stuff in the middle</p> 
+<p>and don’t forget to put a <body> at the bottom like shown</p> 
+<img src="https://www.frugalcouponliving.com/wp-content/uploads/2015/08/ghost-cupcakes-frugal-coupon-living.jpg"height=415 width=320 />
+<p>above is how u do an image</p> 
+<p> 
+
+  
+
+<body>  
