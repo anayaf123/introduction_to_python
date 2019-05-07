1fa3881d8744e515d855aa1fc5e212b219a98017
Salad-CG <s11052057@students.duvalschools.com>
2019-05-07 10:54:05 -0400

    Declared Variables for the fractions.
    Added the numerator and the denominator values  for the two fractions.

---
+++ gardner_connor_multiplying_dividing_fractions_0.3.py
@@ -0,0 +1,14 @@
+# Connor G., Multiplying and Divide Fractions, 05/06/19, version 0.3
+
+print("Hello, my name is Fraction_Bot9000. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? (Type your name and press ENTER.\n")
+print("Hello", user_name," how are you today?")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 - 0
+denominator0 - 0
+# Variables for fraction 1.
+numerator1 - 0
+denominator1 - 0
