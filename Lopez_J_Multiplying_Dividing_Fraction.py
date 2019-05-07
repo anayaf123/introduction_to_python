0867757142469a7f43ac1b1ab7a097b5f8bd0d18
Jayla00 <49207622+Jayla00@users.noreply.github.com>
2019-05-07 14:04:52 -0400

    Created First Version of Program
    Added my first comment to start the program.

---
+++ Lopez_J_Multiplying_Dividing_Fraction.py
@@ -0,0 +1,12 @@
+#Jayla L., Mulitplying and Divide Fractions, 05/06/2019
+
+print("Hello, world my name is Faction-Bot 9000. I will multiply and divide fractions for you.\n")
+print("In order to multiply and divide fractions, I need to know the numerator and denomnator. \n")
+
+numerator0 = 0
+numerator1 = 0
+
+print("The first numerator is", numerator0 " and the second numerator is", numerator1".\n")
+
+denominator0 = 0
+denominator1 = 0 
