26aa8604bdef1114ed7f903a3ba19433f9c14b4f
Salad-CG <s11052057@students.duvalschools.com>
2019-05-07 10:48:14 -0400

    Added user_name variable and input() line.
    added two lines of code to ask for the user's name, save it to the variable using imput() and then printing the name back on the screen.

---
+++ gardner_connor_multiplying_dividing_fractions.py
@@ -0,0 +1,5 @@
+# Connor G., Multiplying and Divide Fractions, 05/06/19, version 0.1
+
+print("Hello, my name is Fraction_Bot9000. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? (Type your name and press ENTER.\n")
+print("Hello", user_name," how are you today?")
