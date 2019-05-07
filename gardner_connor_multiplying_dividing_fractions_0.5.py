637157a144d1eb138cd70e662a669a543ba5ed27
Salad-CG <s11052057@students.duvalschools.com>
2019-05-07 11:08:57 -0400

    Added variables for the new fraction.
    Declared new_numerator and new_denominator, also wrote the code to multiply the two fractions together.

--- gardner_connor_multiplying_dividing_fractions_0.5.py
+++ gardner_connor_multiplying_dividing_fractions_0.5.py
@@ -1,4 +1,4 @@
-# Connor G., Multiplying and Divide Fractions, 05/06/19, version 0.4
+# Connor G., Multiplying and Divide Fractions, 05/06/19, version 0.5
 
 print("Hello, my name is Fraction_Bot9000. I will multiply and divide fractions for you.\n")
 user_name = input("What is your name? (Type your name and press ENTER.\n")
