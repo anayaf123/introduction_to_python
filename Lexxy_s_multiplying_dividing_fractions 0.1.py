5386bd837e0eaa473135239adaee8325a914ab05
Lexxy <s11319001@students.duvalschools.org>
2019-05-08 10:45:58 -0400

    small update added the input for the users name

---
+++ Lexxy_s_multiplying_dividing_fractions 0.1.py
@@ -0,0 +1,14 @@
+#lexxy S., Multiplying and divive fractions, 05/06/19
+
+print("Hello, i am fraction-bot.  I will multiply and divde factions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.] \n")
+print("Hello,", user_name," how are you today? \n")
+print("In order to multiply or divide fractions, Fraction-bot needs to know the numerator and the denominator.\n")
+
+n1 = 0
+n2 = 0
+
+print("the first numerator is", n1,"and the second numerator is")
+
+d1 = 0
+d2 = 0
