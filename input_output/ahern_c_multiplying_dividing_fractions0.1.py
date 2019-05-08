cb12bcb496c7a5efe61770313a51f45b70264909
s11325503 <s11325503@students.duvalschools.org>
2019-05-08 14:10:13 -0400

    changed code in 3rd line
    changed to super math bot51. saving as version 0.1 After first part of video

---
+++ input_output/ahern_c_multiplying_dividing_fractions0.1.py
@@ -0,0 +1,4 @@
+#Ahern C., Multiplying and Dividing Fractions, 05/06/2019, version 0.0
+
+print("Hello, my name is Super math bot51. I will multiply and divide fractions for you.\n")
+print("In order to multiply or divide fractions, I need to know the numerator and the denominator.\n")
