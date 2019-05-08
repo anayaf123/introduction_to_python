e5e66ec2a2e5248fa87103c0e48b6f23a57b1b09
asg0 <s11355187@students.duvalschools.org>
2019-05-08 14:08:28 -0400

    Version 0.4
    Added to print0 lines to print the two fractions on the screen to verify they are correct.

---
+++ multiplying_dividing_fractions_0.4.py
@@ -0,0 +1,17 @@
+# Alani S., Multipling and Dividing Fractions, 05/06/2019, version 0.4
+
+print("Hello, welcome to Fraction-Bot 9000.  I can multiply and divide fractions for you!\n"
+user_name = input("What is your name? [Type your name and press ENTER.]\n ")
+print("Hello," ,user_name,"how are you today?\n")
+
+print("for this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction  0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is",numerator0,"/",denominator0,".\n")
+
+# Variables for fractions 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is",numerator1,"/",denominator1,".\n)
