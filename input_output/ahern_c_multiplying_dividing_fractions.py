9c9b3c997f194018963b5d6ca148c96dc5669b39
s11325503 <s11325503@students.duvalschools.org>
2019-05-08 14:01:34 -0400

    created first version of multiplying and dividing fractions
    made the first code and saved it as a python file.

---
+++ input_output/ahern_c_multiplying_dividing_fractions.py
@@ -0,0 +1,4 @@
+Ahern C., Multiplying and Dividing Fractions, 05/06/2019
+
+"Hello, my name is Fraction-Bot9000. I will multiply and divide fractions for you.\n")
+"In order to multiply or divide fractions, I need to know the numerator and the denominator.\n")
