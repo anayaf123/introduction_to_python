a7697f0d06df43b8ad9edb79c6a7a722f46d7c15
s11325503 <s11325503@students.duvalschools.org>
2019-05-08 14:28:30 -0400

    Added user_name variable and input space
    Added two lines of code for user_name input to ask for the users name and ran file in terminal. it displayed the users name on the screen and asked how they were doing.

--- input_output/ahern_c_multiplying_dividing_fractions0.2.py
+++ input_output/ahern_c_multiplying_dividing_fractions0.2.py
@@ -1,7 +1,7 @@
-#Ahern C., Multiplying and Dividing Fractions, 05/06/2019, version 0.1
+#Ahern C., Multiplying and Dividing Fractions, 05/06/2019, version 0.2
 
 print("Hello, my name is Super math bot51. I will multiply and divide fractions for you.\n")
 user_name = input("What is your name? [Type your name and press ENTER.]\n")
-print("Hello", user_name,"how are you doing at this very moment in time?")
+print("Hello", user_name,"how are you doing at this very moment in time?\n")
 
 print("In order to multiply or divide fractions, I need to know the numerator and the denominator.\n")
