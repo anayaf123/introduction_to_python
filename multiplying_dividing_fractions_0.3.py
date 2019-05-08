ad946ba8288e34509890c9b57edbd08efb7db6d3
asg0 <s11355187@students.duvalschools.org>
2019-05-08 13:59:58 -0400

    Declared Variable for the fractions.
    Added the numerator and denominator values for the two fractions

--- multiplying_dividing_fractions_0.3.py
+++ multiplying_dividing_fractions_0.3.py
@@ -1,4 +1,4 @@
-# Alani S., Multipling and Dividing Fractions, 05/06/2019, version 0.0
+# Alani S., Multipling and Dividing Fractions, 05/06/2019, version 0.3
 
 print("Hello, welcome to Fraction-Bot 9000.  I can multiply and divide fractions for you!\n"
 user_name = input("What is your name? [Type your name and press ENTER.]\n ")
