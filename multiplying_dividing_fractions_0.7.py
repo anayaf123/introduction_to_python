173a34860544cba585b90c67d19a0952c4c9e77f
asg0 <s11355187@students.duvalschools.org>
2019-05-09 14:27:25 -0400

    Starting to Divide Fractions
    Starting the code to divide the fractions entered, explained how to divide using the reciprical. (also not everything is there because a piece of the video is missing)

---
+++ multiplying_dividing_fractions_0.7.py
@@ -0,0 +1,40 @@
+# Alani S., Multipling and Dividing Fractions, 05/06/2019, version 0.7
+
+print("Hello, welcome to Fraction-Bot 9000.  I can multiply and divide fractions for you!\n"
+user_name = input("What is your name? [Type your name and press ENTER.]\n ")
+print("Hello," ,user_name,"how are you today?\n")
+
+print("for this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction  0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is",numerator0,"/",denominator0,".\n")
+
+# Variables for fractions 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is",numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+new_numerator = numerator0 * numerator1 
+new_denomintor = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+denominator0 = int(input("Type the first denominator and press enter.\n")
+denominator1 = int(input("Type the second denominator and press enter.\n")
+
+print("The first fraction is",numerator0,"/",denominator0,".\n")
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
+
+#  This is where the division of the fration will start.
+pie = 22 / 7
+print(pie)
+print("To divide a fraction you will multiply using the reciprical or inverse of the second fraction.\n")
