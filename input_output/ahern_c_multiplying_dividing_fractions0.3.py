a4238aa9ddd07ca714b876817edb38da03d1d480
s11325503 <s11325503@students.duvalschools.org>
2019-05-09 14:02:18 -0400

    Declared variables for the fractions.
    Added the numerator and denominator values for the two fractions.

---
+++ input_output/ahern_c_multiplying_dividing_fractions0.3.py
@@ -0,0 +1,14 @@
+#Ahern C., Multiplying and Dividing Fractions, 05/06/2019, version 0.3
+
+print("Hello, my name is Super math bot51. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello", user_name,"how are you doing at this very moment in time?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
