8dbc51a9bda714fe40d7141815307cc0179b9d40
s11325503 <s11325503@students.duvalschools.org>
2019-05-10 13:02:45 -0400

    Create cja_multiplying_dividing_fractions0.5a.py
    Added my initials to file name

---
+++ input_output/cja_multiplying_dividing_fractions0.5a.py
@@ -0,0 +1,25 @@
+#Ahern C., Multiplying and Dividing Fractions, 05/06/2019, version 0.5
+
+print("Hello, my name is Super math bot51. I will multiply and divide fractions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.]\n")
+print("Hello", user_name,"how are you doing at this very moment in time?\n")
+
+print("For this program, I need to know the numerator and the denominator for both fractions.\n")
+
+# Variables for fraction 0.
+numerator0 = 0
+denominator0 = 0
+print("The first fraction is", numerator0,"/",denominator0,".\n")
+
+# Variables for fraction 1.
+numerator1 = 0
+denominator1 = 0
+print("The second fraction is", numerator1,"/",denominator1,".\n")
+
+print("When multiplying fractions you multiply the two numerators together.\n")
+print("Then you will multiply the two denominators together.\n")
+
+new_numerator = numerator0 * numerator1
+new_denominator = denominator0 * denominator1
+
+print("The new fraction is", new_numerator,"/",new_denominator,".\n")
