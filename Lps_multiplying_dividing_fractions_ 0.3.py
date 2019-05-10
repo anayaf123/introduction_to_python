597000ba3f03144cfc25677d5ed5c1297824bd60
Lexxy <s11319001@students.duvalschools.org>
2019-05-10 15:00:04 -0400

    Create Lps_multiplying_dividing_fractions_ 0.3.py

---
+++ Lps_multiplying_dividing_fractions_ 0.3.py
@@ -0,0 +1,25 @@
+#lexxy S., Multiplying and divive fractions, 05/06/19
+
+print("Hello, i am fraction-bot.  I will multiply and divde factions for you.\n")
+user_name = input("What is your name? [Type your name and press ENTER.] \n")
+print("Hello,", user_name," how are you today? \n")
+print("In order to multiply or divide fractions, Fraction-bot needs to know the numerator and the denominator.\n")
+
+# variables for the first fraction.
+n1 = 0
+d1 = 0
+print("the first fracion is", n1, "/", d1,".\n")
+
+# variables for the second fraction.
+n2 = 0
+d2 = 0
+print("the second fraction is", n2, "/", d2,".\n")
+
+print("when multiplyng fractions you multiply the two numerators together.\n")
+print("then you multipy the two denominators together. \n")
+
+#variables for the new fraction.
+new_numerator = n1 * n2
+new_denominator = d1 * d2
+
+print("the new fracion is", new_numerator, "/", new_denominator,".\n")
