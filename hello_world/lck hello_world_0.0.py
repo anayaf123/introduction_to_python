c38bfb2c6884c246635ca451b295f2f421e865ff
WORM321 <s11375938@students.duvalschools.org>
2019-05-22 13:44:24 -0400

    Create lck hello_world_0.0.py

---
+++ hello_world/lck hello_world_0.0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# Time To Go
+print("hello, welcome to Fraction-Bot 9000.   I can multiply and divide fractions for you!/n")
+user_name = input("What is your name? [Type your name and press ENTER./n")
+print("Hello, ", user_name, "how are you today?")
+
