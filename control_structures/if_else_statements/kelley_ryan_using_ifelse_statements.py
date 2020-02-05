368dd6c4064eefe43723c4536b3cddc30fc6b60e
Ryan Kelley <kelleyr@duvalschools.org>
2020-02-05 00:03:16 -0500

    Version 0.11

--- control_structures/if_else_statements/kelley_ryan_using_ifelse_statements.py
+++ control_structures/if_else_statements/kelley_ryan_using_ifelse_statements.py
@@ -1,4 +1,4 @@
-# Using if / else Statements, Ryan Kelley, February 04, 2020.  Version 0.1
+# Using if / else Statements, Ryan Kelley, February 04, 2020.  Version 0.11
 
 import random # This is a special library built-in to Python.  It has special methods, in this case .shuffle() that I will use in this code. 
 
@@ -22,7 +22,7 @@ print("""
   .----)   |       |  |     /  _____  \   |  |     |  |____ |  |  |  | |  |____ |  |\   |     |  |    .----)   |   
   |_______/        |__|    /__/     \__\  |__|     |_______||__|  |__| |_______||__| \__|     |__|    |_______/    
                                                                                                                    
-                                       by Ryan Kelley, February 04, 2020 Version 0.1
+                                       by Ryan Kelley, February 04, 2020 Version 0.11
 """)
 print("Hello $user, this program will help you learn how to use if / else statements in your code.\n  If you have any questions while using this program please raise your hand.\n")
 user = str(input("$user, by what name should I call you?  Please, type your name and then press ENTER on your keyboard.\n")) # Create a variable named user and assign it a value. 
@@ -38,5 +38,5 @@ print("Your parents really named you",user,"?\n")
 name_correct = input("Is what really what I should call you?  [Y/N?]\n") # Create the variable name_correct and assign it a value. 
 name_correct = name_correct[0] # This assigns the name_correct variable to JUST the first letter of the user's answer. 
 name_correct = str.lower(name_correct) # This assigns the name_correct variable to the lower case version of that first letter.
-print(name_correct) # Testing the code.  
+# It works! print(name_correct) 
 
