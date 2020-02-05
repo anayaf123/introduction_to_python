2f4fc7bd65ed7384ff55913f7f8ce0f96e9d9ce9
Ryan Kelley <kelleyr@duvalschools.org>
2020-02-05 00:27:18 -0500

    Version 0.2
    Re-named user variable to variations of user[#] .  Add .len() method to slice through end of the string for user0.
    
    Finished first if / else statement that checks for a correct user name.

--- control_structures/if_else_statements/kelley_ryan_using_ifelse_statements.py
+++ control_structures/if_else_statements/kelley_ryan_using_ifelse_statements.py
@@ -1,4 +1,4 @@
-# Using if / else Statements, Ryan Kelley, February 04, 2020.  Version 0.15
+# Using if / else Statements, Ryan Kelley, February 04, 2020.  Version 0.21
 
 import random # This is a special library built-in to Python.  It has special methods, in this case .shuffle() that I will use in this code. 
 
@@ -22,33 +22,36 @@ print("""
   .----)   |       |  |     /  _____  \   |  |     |  |____ |  |  |  | |  |____ |  |\   |     |  |    .----)   |   
   |_______/        |__|    /__/     \__\  |__|     |_______||__|  |__| |_______||__| \__|     |__|    |_______/    
                                                                                                                    
-                                       by Ryan Kelley, February 04, 2020 Version 0.15
+                                       by Ryan Kelley, February 04, 2020 Version 0.21
 """)
-print("Hello $user, this program will help you learn how to use if / else statements in your code.\n  If you have any questions while using this program please raise your hand.\n")
-user0 = str(input("$user, by what name should I call you?  Please, type your name and then press ENTER on your keyboard.\n")) # Create a variable named user and assign it a value.
+print("Hello $user, this program will help you learn how to use if / else statements in your code.  If you have any questions while using this program please raise your hand.\n")
+user0 = str(input("$user, by what name should I call you?  Please, type your firt and last name and then press ENTER on your keyboard.\n")) # Create a variable named user and assign it a value.
 # In Python, = is the English equivalent of "Make the thing on the left have the same value as the thing on the right."  
 
 
 # The following lines of code create three slices of the user name and then rearranges them. 
-user1 = user[0:3]
-user2 = user[4:7]
-user3 = user[8:]
+user1 = user0[0:3]
+user2 = user0[4:7]
+user3 = user0[8:len(user0)]
 user4 = user3 + user2 + user1
 
 print("Your parents really named you",user4,"?\n")
-name_correct = input("Is what really what I should call you?  [Y/N?]\n") # Create the variable name_correct and assign it a value. 
+name_correct = input("Is what really what I should call you?  [Yes/No?]\n") # Create the variable name_correct and assign it a value. 
 name_correct = name_correct[0] # This assigns the name_correct variable to JUST the first letter of the user's answer. 
 name_correct = str.lower(name_correct) # This assigns the name_correct variable to the lower case version of that first letter.
 # It works! print(name_correct)
 
 # Our first if / else statement starts here.  It will check the VALUE of the name_correct variable and then test it against each CONDITION.
+
 if name_correct == "y": # == is the English equivalent of "Is the thing on the left the same as the thing on the right?"
-    print("Ok, ",user,"it is!\n")
+    # If they answer yes just leave user0 variable alone and continue on in the program.
+    user0 = user4 
+    print("Ok, ",user0,"it is!\n")
 elif name_correct == "n":
-    print("I am sorry.  I experienced a memory error in one of my circuits.  Please retype your name and press enter.\n")
-    user = str(input())
-    print("Ahh! Yes,",user,"that's more like it!\n")
+    # On the next line we will have the user re-type their name.  
+    user0 = str(input("I am sorry.  I experienced a memory error in one of my circuits.  Please retype your name and press enter.\n"))
+    print("Ahh! Yes,",user0,"that's more like it!\n")
 else:
-    print("Something has caught fire.  Emergency!  Prepare for crash.  Restart the program please.\n")  
+    print("Something has caught fire.  Emergency!  Prepare for crash.  Restart the program please.  Please enter Yes or No only.\n")  
     
 
